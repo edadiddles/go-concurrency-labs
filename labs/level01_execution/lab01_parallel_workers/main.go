8b74109055dc73d8f11bc29d0a144f2a3f4c6975
Edward Ruzek <edadiddles@hotmail.com>
2026-01-29 07:51:15 -0500

    defining durations upfront to prevent concurrency issues with rng and collapsing print statements into a single print statement

--- labs/level01_execution/lab01_parallel_workers/main.go
+++ labs/level01_execution/lab01_parallel_workers/main.go
@@ -22,20 +22,27 @@ func main() {
 
 func parallel_workers(n, low, high int) {
 	var wg sync.WaitGroup
+
+	// define durations upfront
+	durations := make([]int, n)
+	for i := range n {
+		durations[i] = rand.Intn(high-low+1) + low
+	}
 	
 	wg.Add(n)
 	for i := range n {
-		r := rand.Intn(high-low+1) + low
-		go block(i, r, &wg)
+		go block(i, durations[i], &wg)
 	}
 
 	wg.Wait()
 }
 
-func block(i, r int, wg *sync.WaitGroup) {
+func block(i, wait int, wg *sync.WaitGroup) {
 	defer wg.Done()
+	
 
-	fmt.Printf("Worker %d initialized blocking for %dms\n", i, r)
-	time.Sleep(time.Duration(r) * time.Millisecond)
-	fmt.Printf("Worker %d finished\n", i)
+	t0 := time.Now()
+	time.Sleep(time.Duration(wait) * time.Millisecond)
+	t1 := time.Now()
+	fmt.Printf("Worker %d started at %s ended at %s and ran for %dns\n", i, t0, t1, t1.Sub(t0))
 }

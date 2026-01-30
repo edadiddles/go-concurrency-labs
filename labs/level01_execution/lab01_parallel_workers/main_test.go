08d118a1e233583617d1e1a581cb8869de9aaeab
Edward Ruzek <edadiddles@hotmail.com>
2026-01-30 08:08:28 -0500

    added some testing for proof of concurrency and simple checks on start/end time expectations

--- labs/level01_execution/lab01_parallel_workers/main_test.go
+++ labs/level01_execution/lab01_parallel_workers/main_test.go
@@ -20,12 +20,43 @@ func check_stats(n, low, high int, start, end time.Time, stats []Statistics) []s
 		if s.end.Compare(max_end) > 0 {
 			max_end = s.end
 		}
-		sequential_duration = sequential_duration + (s.end.Sub(s.start).Milliseconds())
+		worker_duration := int64(s.end.Sub(s.start).Milliseconds())
+		sequential_duration = sequential_duration + worker_duration
+
+		// check start < end
+		if s.start.Compare(s.end) >= 0 {
+			errs = append(errs, "start time does not occur before end time")
+		}
+
+		// check duration >= low
+		if s.end.Sub(s.start).Milliseconds() < int64(low) {
+			errs = append(errs, "duration does not exceed minimum value")
+		}
 	}
 
 	// check N workers completed
 	if len(stats) != 10000 {
 		errs = append(errs, "Not all logs returned")
+	}
+
+	// check total measured runtime does not exceed max duration by an appreciable amount.
+	//if end.Sub(start).Milliseconds() > int64(high + 10)  {
+	//	errs = append(errs, "measured runtime exceeded expectations")
+	//}
+
+	// check for proof of concurrency
+	is_concurrent := false
+	for i := 1; i < len(stats); i++ {
+		for j := 0; j < i; j++ {
+			if stats[i].start.Compare(stats[j].start) < 0 && stats[j].end.Compare(stats[i].end) < 0 {
+				is_concurrent = true
+			} else if stats[i].start.Compare(stats[j].start) > 0 && stats[j].end.Compare(stats[i].end) > 0 {
+				is_concurrent = true
+			}
+		}
+	}
+	if !is_concurrent {
+		errs = append(errs, "proof of concurrency dne")
 	}
 
 	return errs

dc3c0a77da3044c8881dbd53cd0c517c6f066540
Edward Ruzek <edadiddles@hotmail.com>
2026-01-31 11:07:06 -0500

    init attempt at lab03 logic

--- labs/level02_shared_state/lab03_shared_counter/main.go
+++ labs/level02_shared_state/lab03_shared_counter/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	fmt.Println("hello lab03")
+
+	n, _ := strconv.Atoi(os.Args[1])
+	k, _ := strconv.Atoi(os.Args[2])
+
+	count := shared_counter(n, k)
+
+	fmt.Println("current count:", count)
+}
+
+
+func shared_counter(n, k int) int {
+	counter := 0
+	for range n {
+		go increment_counter(&counter, k)
+	}
+	return counter
+}
+
+func increment_counter(cnt *int, k int) {
+	for range k {
+		*cnt += 1
+	}
 }

e11734cbc83441d3efee2f3d2ece6913d8425bf2
Edward Ruzek <edadiddles@hotmail.com>
2026-02-12 08:27:03 -0500

    adding tests for lab11

--- labs/level05_cancellation/lab11_cancellation/main_test.go
+++ labs/level05_cancellation/lab11_cancellation/main_test.go
@@ -2,8 +2,85 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
-func TestEmpty(t *testing.T) {
-	t.Error("not implemented")
+func check_report(report CancelReport, n int) []string {
+	errs := make([]string, 0)
+
+	cancel_time := time.Now()
+	if !report.cancel_time.IsZero(){
+		cancel_time = report.cancel_time
+	}
+
+	if len(report.worker_reports) > n {
+		errs = append(errs, "too many worker reports")
+	} else if len(report.worker_reports) < n {
+		errs = append(errs, "missing worker reports")
+	}
+
+
+	for _, worker_report := range report.worker_reports {
+		if worker_report.exit_time.Compare(cancel_time) > 0 && !worker_report.interrupted {
+			errs = append(errs, "worker finished after cancellation without interruption")
+		} else if worker_report.exit_time.Compare(cancel_time) < 0 && worker_report.interrupted {
+			errs = append(errs, "worker interrupted before cancellation")
+		}
+
+	}
+	return errs
+}
+
+func TestBasic(t *testing.T) {
+
+	n := 10
+	k := 10
+	p := 300
+	low := 10
+	high := 50
+
+	for range 10 {
+		report := cancellation(n, k, p, low, high)
+
+		errs := check_report(report, n)
+		for err := range errs {
+			t.Error(err)
+		}
+	}
+}
+
+func TestManyWorkers(t *testing.T) {
+
+	n := 100
+	k := 10
+	p := 300
+	low := 10
+	high := 50
+
+	for range 10 {
+		report := cancellation(n, k, p, low, high)
+
+		errs := check_report(report, n)
+		for err := range errs {
+			t.Error(err)
+		}
+	}
+}
+
+func TestManyWorkUnits(t *testing.T) {
+
+	n := 10
+	k := 100
+	p := 3000
+	low := 10
+	high := 50
+
+	for range 10 {
+		report := cancellation(n, k, p, low, high)
+
+		errs := check_report(report, n)
+		for err := range errs {
+			t.Error(err)
+		}
+	}
 }
